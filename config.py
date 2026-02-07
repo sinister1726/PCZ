cb02d4caf0f6dac89c3371cb80e5a91f3079cfbf
drexocoder-source <drexocoder@gmail.com>
2026-02-07 18:13:12 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,24 @@
+import os
+
+class Config:
+    API_ID = 25887786
+    API_HASH = "e4201277f5f2883f22c150167bd24479"
+    BOT_TOKEN = "8264820868:AAGfpaMQ0uiO_N0rfCskR6Jfq0uTqhoe-Kk"
+
+    DATABASE_URL = (
+        "postgresql://neondb_owner:npg_yNzl0oqVOw4M@ep-billowing-frost-a186v8hy.ap-southeast-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
+    )
+    BOT_USERNAME = "@CricketLegacy2Bot"
+    SUPPORT_GROUP = "https://t.me/CLG_fun_zone"
+    PLAY_ZONE_INFO = "https://t.me/CLG_fun_zone"
+    OWNER_IDS = {8084629653} 
+
+    NVIDIA_API_KEY = "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"
+
+
+    START_IMAGE = "https://graph.org/file/4bb1b361de5ad086c9978-e0b4b80898c072471f.jpg"
+
+
+    LOG_CHANNEL = -1003527724170
+
+
