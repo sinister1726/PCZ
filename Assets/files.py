7c4216f754942ee26d9668296a64064a88b0696c
drexocoder-source <drexocoder@gmail.com>
2026-02-07 18:30:35 +0530

    Add files via upload

---
+++ Assets/files.py
@@ -0,0 +1,46 @@
+START_IMAGE_GROUP = "https://graph.org/file/3aa791b73d748431723d1-7220b5d7a854613e46.jpg"
+TEAM_CREATE_IMAGE = "https://graph.org/file/688b6ec01fbe089bb55aa-64c5fba5c8c2b450e9.jpg"
+SOLO_MODE_IMAGE = "https://graph.org/file/4226b2dfedbd17875e264-0d036518cc600421e8.jpg"
+MEMBERS_IMAGE = "https://graph.org/file/c329b8658cc1eccfe2ecb-9f8bf0795204be616e.jpg"
+PROFILE_IMG = "https://graph.org/file/6305d8849e0664deb7a58-92bbcd38d61e019fa4.jpg"
+LEADERBOARD_IMG = "https://graph.org/file/d7483c8e9080fa9e7cce6-949ee0e62b81dbed49.jpg"
+ACHIEVE_IMG = "https://graph.org/file/fb505142db17ba751f556-c7cbc9c34632a5af33.jpg"
+
+
+RUN_VIDEOS = {
+    "0": [
+        "BAACAgEAAxkBAAIB-2luCWBamXsf6KSUHcO8RqZIykxtAAIYCgACOIFwR7SgBHPlG4i_HgQ"
+    ],
+    "1": [
+        "BAACAgEAAxkBAAIB72luCJ3emSAqDomx7I_oudI3Oa9DAAImCgACOIFwR30bt7pvXqj1HgQ"
+    ],
+    "2": [
+        "CgACAgEAAxkBAAMUaWuIk3rPj6TIp1f-w0zOI-K7YmgAAnoFAAJ8W1lH0e79cNeSdFoeBA",
+        "BAACAgEAAxkBAAIB8mluCMadFWm-fb_K9GhLkoGKb7lpAAIjCgACOIFwR2Kvb3xKy1D5HgQ"
+    ],
+    "3": [
+        "BAACAgEAAxkBAAOlaWyIpi1lQibiPQX-dR8Okw-GwXQAAtMEAAJDr2lH6RxvEF6ZDyAeBA"
+    ],
+    "4": [
+        "BAACAgEAAxkBAAOiaWyIXTlIrNJvddpBwpXSHHOeOzkAAtIEAAJDr2lHzkSQ9x5h2wQeBA",
+        "BAACAgEAAxkBAAICHWluC5zZSubBsf1ZGn20BCNbdl97AAIrCgACOIFwR6JBVM4FIPWAHgQ"
+    ],
+    "5": [
+        "BAACAgEAAxkBAAMoaWuZXSuVcj1xPiwXn7rQ9LO2vjoAAs4HAAJDr2FHAeGsatQJWrIeBA",
+        "CgACAgEAAxkBAANyaWxkOLa8GiFTjeftu4bH5kCYBtIAAsEEAAJDr2lHSTGaU5zovzMeBA"
+    ],
+    "6": [
+        "CgACAgEAAxkBAAN1aWxkVmme_zPYWbiGcvG79P1iJSIAAsIEAAJDr2lHd54khb0aTNgeBA",
+        "BAACAgEAAxkBAAIB-GluCS6MD8Kdoo4voQxIx2p-GQXbAAIfCgACOIFwR9WpKNSP-EzQHgQ"
+    ],
+    "Out": [
+        "BAACAgEAAxkBAAOfaWyIQmaU95kHweaviv6zuN1-p3QAAtEEAAJDr2lHvgSV16zS5W0eBA",
+        "BAACAgEAAxkBAAOoaWyI1b1nZW1NfCYdNf84TISPo5AAAtQEAAJDr2lHzGZSUlL_rUUeBA",
+        "BAACAgEAAxkBAAIChmluFb3bru6KEMprvvhb4LZZNMPWAAI2CgACOIFwR-a7qPS5e1-yHgQ"
+    ],
+    "Batting": [ "BAACAgEAAxkBAAOraWyJENRxMF0V3JwGuvkqy02n_p4AAtUEAAJDr2lHJ4ZPTDFwtsceBA", ],
+    "Bowling": [ "BAACAgEAAxkBAAMsaWuZhdw3n9JEZpBUPDH6T-J-JwoAAusGAAInrEFHrY3sqPEFBvQeBA", ],
+    "Opening": [ "BAACAgEAAxkBAAIB9WluCP9GK0OLtfReSwQ_HnDtlGXZAAIlCgACOIFwR12NFmuc0f0fHgQ",]
+}
+
+
