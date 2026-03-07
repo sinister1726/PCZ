56493873ef65d8420f7dfad90ac4d02c3d1d2c02
drexocoder-source <drexocoder@gmail.com>
2026-03-07 11:44:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ class Config:
     PLAY_ZONE_INFO = "https://t.me/+joF1bCfiMT9jMzVh"
     OWNER_IDS = {8585494095} 
     GITHUB_TOKEN = os.getenv("GITHUB_TOKEN")
-    UPSTREAM_REPO = f"https://{GITHUB_TOKEN}@github.com/drexocoder-source/Cricsssketlegssacys"
+    UPSTREAM_REPO = f"https://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@github.com/drexocoder-source/Cricsssketlegssacys"
     NVIDIA_API_KEY = "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"
 
     START_IMAGE = "https://graph.org/file/a37d935e98e4c92e04cee-c1871cfafb3f808563.jpg"
